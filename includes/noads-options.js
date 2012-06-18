aa28c1a40dbf4d3d3664fcadb0900036f66e0f77
Dither <ltrapper@gmail.com>
2012-06-18 19:33:21 +0400

    Corrected RegExp for whitelisting scripts

--- includes/noads-options.js
+++ includes/noads-options.js
@@ -959,7 +959,7 @@ var options = {
                 var textarea = document.getElementById('noads_jsblocks_textarea');
                 var val = textarea.value.substring(textarea.selectionStart, textarea.selectionEnd).replace(/^\s+|\r|\s+$/g, '');
                 if (val) {
-                    val = val.replace(/[*+?^=!${}()|[\]\\]|\.(?!\w)/g, '\\$&').replace(/\n+/g, '|');
+                    val = val.replace(/[*+?^=!${}()\.|[\]\\]|\.(?!\w)/g, '\\$&').replace(/\n+/g, '|').replace(/http:/gi,'^https?:');
                     var whitelist = getValue('noads_scriptlist_white');
                     setValue('noads_scriptlist_white', '@@==' + val + (whitelist ? '\n' + whitelist : ''));
                     alert(lng.pBlockedAdded + ' ' + val);
