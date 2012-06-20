146ba89c7169f423349cd4306558cd470f2ecfc3
Dither <ltrapper@gmail.com>
2012-06-20 21:04:32 +0400

    Message should be encoded.

--- scripts/noads-background.js
+++ scripts/noads-background.js
@@ -33,7 +33,7 @@ window.addEventListener('load', function () {
         var atab = opera.extension.tabs.getFocused();
         if (v12) {
             button.disabled = true;
-            if (!!atab.port) atab.port.postMessage({ type: 'ask_status' });
+            if (!!atab.port) atab.port.postMessage(encodeMessage({ type: 'ask_status' }));
         } else {
             button.disabled = !!atab;
         }
